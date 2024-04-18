2dbf54a12bf4f71ac168908459fca96be8ec5e41
kimbochen <chentenghung@gmail.com>
2024-04-18 16:01:52 -0700

    Fixed fronted bug and reordering docker build stages.

--- frontend/launch_frontend.py
+++ frontend/launch_frontend.py
@@ -9,11 +9,6 @@ USER_ID = sys.argv[1]
 QA_ID = None
 
 
-def log_good_ans(q_str, a_str):
-    requests.post(f'{API_ENDPOINT}/feedback', params={'qa_id': QA_ID, 'user_id': USER_ID})
-    return gr.update(interactive=False)
-
-
 def query_aqua(query):
     global QA_ID, USER_ID
     response = requests.get(API_ENDPOINT, params={'query': query, 'user_id': USER_ID}).json()
@@ -21,10 +16,28 @@ def query_aqua(query):
     return response['answer']
 
 
+def flag_response(q_str, a_str):
+    requests.post(f'{API_ENDPOINT}/feedback', params={'qa_id': QA_ID, 'user_id': USER_ID})
+    return gr.update(interactive=False)
+
+
+def select_index(qtype_val):
+    show_textbox = (qtype_val == 'Assignment')
+    return (
+        gr.update(visible=show_textbox, interactive=show_textbox),
+        gr.update(visible=show_textbox, interactive=show_textbox)
+    )
+
+
 def main():
     theme = gr.themes.Default(spacing_size='lg', text_size='lg')
 
-    with gr.Blocks(theme=theme) as qabot:
+    with gr.Blocks(theme=theme) as aqua:
+        with gr.Row():
+            qtype = gr.Dropdown(['General', 'Assignment'], label='Question Type')
+            asmt_id = gr.Textbox(label='Assignment No.', visible=False)
+            q_id = gr.Textbox(label='Question No.', visible=False)
+
         qbox = gr.Textbox(label='Enter your question')
         abox = gr.Textbox(label='Answer', interactive=False, autoscroll=False, max_lines=12)
 
@@ -33,14 +46,20 @@ def main():
             clear_btn = gr.ClearButton(components=[qbox, abox], size='sm')
             flag_btn = gr.Button('Good Response', size='sm', interactive=False)
 
+        # Question type logic
+        qtype.input(select_index, inputs=[qtype], outputs=[asmt_id, q_id])
+
+        # Question submission logic
         gr.on(
             triggers=[qbox.submit, submit_btn.click], fn=query_aqua,
             inputs=qbox, outputs=abox
         )
-        qbox.change(lambda q_str: gr.update(interactive=(q_str != '')), inputs=qbox, outputs=flag_btn)
-        flag_btn.click(log_good_ans, inputs=[qbox, abox], outputs=flag_btn)
 
-    qabot.launch(
+        # Flag button logic
+        qbox.change(lambda q_str: gr.update(interactive=(q_str != '')), inputs=qbox, outputs=flag_btn)
+        flag_btn.click(flag_response, inputs=[qbox, abox], outputs=flag_btn)
+
+    aqua.launch(
         server_name='0.0.0.0',
         root_path=f'{USER_ID}/aqua',
         server_port=int(sys.argv[2])
